c2c9d2152860c6040773d2596cf6bb51f011cc79
Javed <javedahmednizamani@gmail.com>
2025-09-06 22:00:11 +0500

    triangle

---
+++ turtleGraphics/p1_triangle.py
@@ -0,0 +1,10 @@
+import turtle
+
+my_turtle = turtle.Turtle(shape = "turtle")
+
+my_turtle.forward(200)
+my_turtle.left(120)
+my_turtle.forward(200)
+my_turtle.left(120)
+my_turtle.forward(200)
+turtle.exitonclick()
